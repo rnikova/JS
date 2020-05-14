5b731f86669b9c77cac9ac9565de0e81f042d2f7
Rosmari Nikova <rosmari.nikova@yahoo.com>
2020-05-14 21:13:18 +0300

    Add DOM homework

--- JS Advanced/DOM/07. Number-Convertor/solution.js
+++ JS Advanced/DOM/07. Number-Convertor/solution.js
@@ -1,4 +1,32 @@
 function solve() {
+	let selectMenuTo = document.getElementById('selectMenuTo');
+	let binaryOption = document.createElement('option');
+	binaryOption.textContent = 'Binary';
+	binaryOption.value = 'binary';
 
-    //TODO...
+	let hexadecimalOption = document.createElement('option');
+	hexadecimalOption.textContent = 'Hexadecimal';
+	hexadecimalOption.value = 'hexadecimal';
+
+	selectMenuTo.appendChild(binaryOption);
+	selectMenuTo.appendChild(hexadecimalOption);
+
+	let button = document.getElementsByTagName('button')[0];
+	button.addEventListener('click', convert);
+
+	function convert(){
+		let number = Number(document.getElementById('input').value);
+       		let system = selectMenuTo.value;
+       		let result;
+       
+       		if(system == 'binary'){
+            		result = number.toString(2);            
+       		}else{
+         		result=('0' + (number.toString(16))).slice(-2).toUpperCase();
+       		}
+       		if(result){
+           		let resultDiv = document.getElementById('result');
+           		resultDiv.value = result;        
+       		}
+	}
 }
