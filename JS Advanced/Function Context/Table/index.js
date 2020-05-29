d98d72bbd273b43508855cd2bc0e90a65417bd06
Rosmari Nikova <rosmari.nikova@yahoo.com>
2020-05-29 22:17:45 +0300

    Add Table and NextArticle homework

---
+++ JS Advanced/Function Context/Table/index.js
@@ -0,0 +1,19 @@
+function solve() {
+    Array.from(
+            document.getElementsByTagName('tr')
+        ).slice(1)
+        .forEach(tr => tr.addEventListener('click', function () {
+            if (this.hasAttribute('style')) {
+                this,
+                this.removeAttribute('style');
+            }
+            else {
+                Array.from(this.parentElement.children)
+                    .forEach(row => {
+                        row.removeAttribute('style')
+                    });
+
+                    this.style.backgroundColor = '#413f5e';
+            }
+        }))
+}
