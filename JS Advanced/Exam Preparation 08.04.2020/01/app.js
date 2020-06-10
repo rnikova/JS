1da607cacd985a617ad5488202c0a4aed9d118d8
Rosmari Nikova <rosmari.nikova@yahoo.com>
2020-06-10 23:23:47 +0300

    Started Exam Prearation 08.04.2020

--- JS Advanced/Exam Preparation 08.04.2020/01/app.js
+++ JS Advanced/Exam Preparation 08.04.2020/01/app.js
@@ -1,3 +1,37 @@
 function solve() {
-    console.log("//TODO")
+    let task = document.getElementById('task');
+    let description = document.getElementById('description');
+    let date = document.getElementById('date');
+    let addBtn = document.querySelector('button#add').addEventListener('click', AddTask);
+
+    function AddTask(e) {
+        e.preventDefault();
+        if(task.value !== "" || description.value !== "" || data.value !== ""){
+            let openSection = document.querySelector('.orange').parentNode.parentNode.children[1];
+            let openTask = document.createElement('article');
+            let h3Task = document.createElement('h3');
+            h3Task.textContent = task.value;
+            let pDescription = document.createElement('p');
+            pDescription.innerText = 'Description: ' + description.value;
+            let pDate = document.createElement('p');
+            pDate.innerText = 'Due Date: ' + date.value;
+            let divBtns = document.createElement('div')
+            divBtns.className = 'flex';
+            let greenBtn = document.createElement('button');
+            greenBtn.className = 'green';
+            greenBtn.textContent = 'Start';
+            let redBtn = document.createElement('button');
+            redBtn.className = 'red';
+            redBtn.textContent = 'Delete';
+
+            divBtns.appendChild(greenBtn);
+            divBtns.appendChild(redBtn);
+            openTask.appendChild(h3Task);
+            openTask.appendChild(pDescription);
+            openTask.appendChild(pDate);
+            openTask.appendChild(divBtns);
+            openSection.appendChild(openTask);
+        }
+    }
+
 }
