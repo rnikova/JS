6ceddd4def4140b62f89e02744d740218306f2a1
Rosmari Nikova <rosmari.nikova@yahoo.com>
2020-07-02 21:56:58 +0300

    Add bus schedule problem

--- JS Applications/REST Services and AJAX/02.BUS-SCHEDULE/app.js
+++ JS Applications/REST Services and AJAX/02.BUS-SCHEDULE/app.js
@@ -1,13 +1,44 @@
 function solve() {
 
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+    const span = document.getElementsByClassName('info')[0];
+
+    let nextStop = 'depot';
+    let currentStop = '';
+
     function depart() {
-        console.log('Depart TODO...');
+        fetch(`https://judgetests.firebaseio.com/schedule/${nextStop}.json`)
+            .then(chechForErrors)
+            .then(res => res.json())
+            .then(action)
+            .catch(errorHandling)
+        function chechForErrors(res) {
+            if (res.ok === false) {
+                throw new Error(`${res.status} - ${res.statusText}`);
+            }
+            return res;
+        }
+        function errorHandling(err) {
+            span.textContent = 'Error';
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+            console.error(err);
+        }
+        function action({ name, next }) {
+            nextStop = next;
+            currentStop = name;
+            span.textContent = `Next stop ${name}`;
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+        }
     }
-
     function arrive() {
-        console.log('Arrive TODO...');
+        span.textContent = `Arriving at ${currentStop}`;
+        currentStop = nextStop;
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
     }
-
     return {
         depart,
         arrive
