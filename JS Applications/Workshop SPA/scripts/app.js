4a1505a54cd75125e734aa92e07947a4b174891a
Rosmari Nikova <rosmari.nikova@yahoo.com>
2020-07-14 23:07:30 +0300

    Refactor app.js and add home.hbs

--- JS Applications/Workshop SPA/scripts/app.js
+++ JS Applications/Workshop SPA/scripts/app.js
@@ -1,77 +1,37 @@
 import {get, post, put, del} from './requester.js';
+import * as authHandler from './handlers/auth-handler.js';
+import * as shared from './shared.js'
 
 (() => {
     const app = Sammy('#rooter', function (){
         this.use('Handlebars', 'hbs');
 
         this.get('/', function (ctx) {
-            setHeaderInfo(ctx);
-            this.loadPartials(getPartials())
-                .partial('./views/recipes/home.hbs');
-        });
-
-        this.get('/register', function(ctx){
-            this.loadPartials(getPartials())
-            .partial('./views/auth/register.hbs');
-        });
-
-        this.post('/register', function(ctx){
-            const { firstName, lastName, username, password, repeatPassword } = ctx.params;
-
-            if(firstName && lastName && username && password && password === repeatPassword){
-                post('user', '', {firstName, lastName, username, password}, 'Basic')
-                    .then((userInfo) => {
-                        saveAuthInfo(userInfo);
-                        ctx.redirect('/');
-                    })
-                    .catch(console.error);
+            shared.setHeaderInfo(ctx);
+            const partials = shared.getPartials();
+            if(ctx.isAuth){
+                get('appdata', 'recipes', 'Kinvey')
+                    .then((recipes) => {
+                        ctx.recipes = recipes;
+                        this.loadPartials(partials)
+                            .partial('./views/recipes/home.hbs');
+                    });
+            }else{
+                partials['homeAnonymous'] = './views/recipes/homeAnonymous.hbs';
+                this.loadPartials(partials)
+                    .partial('./views/recipes/home.hbs');
             }
         });
 
-        this.get('/login', function(ctx){
-            this.loadPartials(getPartials())
-            .partial('./views/auth/login.hbs');
-        });
+        this.get('/register', authHandler.getRegister);
 
-        this.post('/login', function(ctx){
-            const {username, password} = ctx.params;
+        this.post('/register', authHandler.postRegister);
 
-            if(username && password){
-                post('user', 'login', {username, password}, 'Basic')
-                    .then((userInfo) => {
-                        saveAuthInfo(userInfo);
-                        ctx.redirect('/');
-                    })
-                    .catch(console.error);
-            }
-        });
+        this.get('/login', authHandler.getLogin);
 
-        this.get('/logout', function(ctx){
-            post('user', '_logout', {}, 'Kinvey')
-                .then(() => {
-                    sessionStorage.clear();
-                    ctx.redirect('/');
-                })
-                .catch(console.error);
-        });
+        this.post('/login', authHandler.postLogin);
 
-        function getPartials(){
-            return{
-                header: './views/common/header.hbs',
-                footer: './views/common/footer.hbs'
-            }
-        }
-
-        function setHeaderInfo(ctx){
-            ctx.isAuth = sessionStorage.getItem('authtoken') !== null;
-            ctx.fullName = sessionStorage.getItem('fullName');
-        }
-
-        function saveAuthInfo(userInfo){
-            sessionStorage.setItem('authtoken', userInfo._kmd.authtoken);
-            sessionStorage.setItem('fullName', userInfo.firstName + ' ' + userInfo.lastName);
-        }
+        this.get('/logout', authHandler.logout);
     });
-
     app.run();
 })()
